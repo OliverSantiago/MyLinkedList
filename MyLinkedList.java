9623027183eb78fd6f96ee5a0fe596d601044e74
OliverSantiago <osantiagokermani20@stuy.edu>
2021-01-06 15:53:30 -0500

    initial

---
+++ MyLinkedList.java
@@ -0,0 +1,22 @@
+public class MyLinkedList{
+  private int size;
+  private Node start,end;  
+  public MyLinkedList(){
+    /*create a constructor*/
+    size = 0;
+    start = null;
+    end = null;
+  }
+  public int size(){
+    return size;
+  }
+  public boolean add(String value){
+    return true;
+  }
+  
+  public boolean add(int index, String value);
+  public String get(int index);
+  public String set(int index, String value);
+  public String toString();
+  //Any helper method that returns a Node object MUST BE PRIVATE!
+}
