6bc2e31eeb7dac9b7f0b25952181ce34e07d3607
OliverSantiago <osantiagokermani20@stuy.edu>
2021-01-06 15:44:31 -0500

    get

---
+++ Node.java
@@ -0,0 +1,24 @@
+public class Node{
+  
+  private String data;
+  private Node next,prev;
+   
+  public Node(String value){
+    data = value;
+    next = null;
+    prev = null;
+  }
+//write get/set methods for all three instance variables.
+  
+  public String getData(){
+    return data;
+  }
+  
+  public Node getNext(){
+    return next;
+  }
+  
+  public Node getPrev(){
+    return prev;
+  }
+}
